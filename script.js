74995afc03ca87ebd5fa2c933177001899fc0586
Ana <atendimento@zaniniinfo.com.br>
2020-09-08 21:36:55 -0300

    tipos primitivos js

---
+++ script.js
@@ -0,0 +1,52 @@
+/*
+var boolean = false; 
+console.log(typeof(boolean));
+console.log(boolean);
+
+numero = 1;
+console.log(typeof(numero));
+console.log(numero);
+
+numero = Math.PI;
+console.log(typeof(numero));
+console.log(numero);
+
+var texto = 'texto';
+console.log(typeof(texto));
+console.log(texto);
+
+var n2 = 10;
+var n1 = 20;
+
+console.log('soma',n1+n2);
+console.log('subtração',n1-n2);
+console.log('divisao',n1/n2);
+console.log('multiplicação',n1+n2);
+
+console.log('potencia n1 elevado a n2', Math.pow(2,5));
+console.log('raiz quadrada de 25', Math.sqrt(25));
+
+var pi = Math.PI;
+var numeroRandomico = Math.random() *100;
+
+console.log('Numero randomico', Math.floor(numeroRandomico));
+*/
+
+var texto = 'Texto';
+
+console.log(texto.toLocaleLowerCase);
+console.log(texto.toUpperCase);
+
+var texto = '  Texto  ';
+console.log(texto.trim);
+
+var texto = 'texto';
+console.log(texto.startsWith('tex'));
+console.log(texto.endsWith('to'));
+console.log(texto.search('x'));
+console.log(texto.charAt(0));
+console.log(texto[0]);
+console.log(texto.concat(' texto2'));
+
+
+
